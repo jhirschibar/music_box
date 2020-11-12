fba140fa844cdfdeaa60956644f5228661e11ac9
HirschiBar <jhirschibar@gmail.com>
2020-11-12 17:55:02 -0500

    added code to containerize the app

--- project_ui3.0/app.py
+++ project_ui3.0/app.py
@@ -61,4 +61,5 @@ def getMyJson():
     return(response)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3001, debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    #app.run(host='127.0.0.1', port=3001, debug=True)
