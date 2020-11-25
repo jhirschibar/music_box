b89580f620020758bae1b1cf7d2cfb1f9bf501d8
HirschiBar <jhirschibar@gmail.com>
2020-11-25 10:54:31 -0500

    repo clean up

--- project_ui/forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, StringField, SelectField
-class MusicSearchForm(Form):
-    choices = [('Song', 'Song'),
-               ('Artist', 'Artist')]
-    select = SelectField('Search for music:', choices=choices)
-    search = StringField('')
